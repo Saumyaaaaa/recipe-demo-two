c69927297ddfb030ba9023f5ff3d316189d7d1f9
SAUMYA NEUPANE <105053199+Saumyaaaaa@users.noreply.github.com>
2023-10-19 17:14:52 +0545

    added a category folder i

---
+++ app/category/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hhhf
+    </div>
+  )
+}
+
+export default page
--- app/recipelist/[area]/page.jsx
+++ app/recipelist/[area]/page.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
 
-const page = () => {
+
+const page = async({
+    params
+}) => {
+    async function fetchcount(){
+        const response= await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${params.area}`)
+        return response.json()
+
+    }
+    const area=await fetchcount()
+    console.log(fetchcount)
+
+    
+
   return (
     <div>
-      
+      {
+      area.meals.map((items)=>
+      <li>
+        {items.strMeal}
+      </li>
+      )
+      }
+
     </div>
+
   )
 }
 
